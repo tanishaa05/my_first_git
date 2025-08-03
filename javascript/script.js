f733271b36237be8df3d3a6b1555e0a3cc9e94c0
Tanisha Dutta <iamtanishadutta@gmail.com>
2025-08-04 00:05:52 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+<<<<<<<< HEAD:project/javascript/script.js
+// Smooth scroll to "features" section on button click
+document.getElementById("cta-button").addEventListener("click", function () {
+  document.getElementById("features").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+========
+// Smooth scroll to "features" section on button click
+document.getElementById("cta-button").addEventListener("click", function () {
+  document.getElementById("features").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+>>>>>>>> ee1a40e620f26a95f84bf408eca4b666b2140dfa:script.js
