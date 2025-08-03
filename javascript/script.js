ce440adc1c1779a1168f809e7c7d3c5ac71a852e
Tanisha Dutta <iamtanishadutta@gmail.com>
2025-08-04 00:10:32 +0530

    conflict

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,7 @@
-<<<<<<<< HEAD:project/javascript/script.js
-// Smooth scroll to "features" section on button click
-document.getElementById("cta-button").addEventListener("click", function () {
-  document.getElementById("features").scrollIntoView({
-    behavior: "smooth"
-  });
-});
-========
-// Smooth scroll to "features" section on button click
-document.getElementById("cta-button").addEventListener("click", function () {
-  document.getElementById("features").scrollIntoView({
-    behavior: "smooth"
-  });
-});
 
->>>>>>>> ee1a40e620f26a95f84bf408eca4b666b2140dfa:script.js
+// Smooth scroll to "features" section on button click
+document.getElementById("cta-button").addEventListener("click", function () {
+  document.getElementById("features").scrollIntoView({
+    behavior: "smooth"
+  });
+});
