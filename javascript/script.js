17fa8896055dace396cba3dc11e34a67998aa3a8
Tanisha <iamtanishadutta@gmail.com>
2025-08-07 00:59:24 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,4 @@
 
-// Smooth scroll to "features" section on button click
 document.getElementById("cta-button").addEventListener("click", function () {
   document.getElementById("features").scrollIntoView({
     behavior: "smooth"
